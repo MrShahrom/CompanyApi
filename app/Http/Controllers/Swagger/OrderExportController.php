f3b08479aeee7f1bd9f9d36ef7995a95af965760
MrShahrom <shahromeo778@gmail.com>
2024-02-23 13:35:20 +0500

    fixed orderexport

--- app/Http/Controllers/Swagger/OrderExportController.php
+++ app/Http/Controllers/Swagger/OrderExportController.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\Controller;
  * @OA\Get(
  *     path="/api/order_export",
  *     summary="Экспорт в excel",
- *     tags={"Cost"},
+ *     tags={"Order"},
  *     @OA\Response(
  *         response=200,
  *         description="Ok"
